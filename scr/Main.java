da7f07d014f83bf149558871cb3203e96908630b
BBL09 <100301471+BBL09@users.noreply.github.com>
2022-03-03 22:23:01 +0300

    hello my git

--- scr/Main.java
+++ scr/Main.java
@@ -1,5 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println(123);
+        while (true){
+            int food = getPriceFromDataBase();
+            dominoFastFood(food);
+        }
+    }
+    private static int getPriceFromDataBase() {
+        System.out.print("Введите сумму:$- ");
+        Scanner input = new Scanner(System.in);
+        int food = input.nextInt();
+        return food;
+    }
+    private static int dominoFastFood(int food){
+        if (food < 100){
+            System.out.println("döner");
+        }else if (food <=300){
+            System.out.println("kebap");
+        }else if (food <=500){
+            System.out.println("Шаурма");
+        }else {
+            System.out.println("Пицца");
+        }
+        return food;
     }
 }

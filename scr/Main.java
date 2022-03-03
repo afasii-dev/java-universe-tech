badef4b112b5c38a51b7f52eb01575d6568c8e0b
Rustam0330 <Rustam.Khodjaeff@gmail.com>
2022-03-03 11:44:21 -0500

    add variables

--- scr/Main.java
+++ scr/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
+        String brand = "Mercedes";
+        String model = "W221"
+        int year = 2019;
+        System.out.println(brand + model + year);
     }
 }

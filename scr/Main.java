ecc2e5f0d17cd8da32f26a4654aa5e632357e459
sega-11 <sega111183@mail.ru>
2022-03-04 14:36:21 +0500

    first commit

--- scr/Main.java
+++ scr/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
+
+
+        int sega = 11;
+        int world = 11;
+        System.out.println(sega + world);
+
     }
 }

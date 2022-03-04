c7cce6208e2b4e9feb17ad4d3054f6e655c0d597
sega-11 <sega111183@mail.ru>
2022-03-04 14:49:29 +0500

    second commit

--- scr/Main.java
+++ scr/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        String vesna = "hello vesna";
         int sega = 11;
         int world = 11;
         System.out.println(sega + world);
+        System.out.println(vesna);
 
     }
 }

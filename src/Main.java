3090a20fe08a8947162c4f51a126697418303f1c
alexandr.fasii <afasii.dev@yahoo.com>
2022-03-07 18:04:40 +0200

    first

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello BBL!");
-        String name = "Rustam";
+        String name = "123";
         System.out.println(name);
+        int i = 10;
+        System.out.println(i);
     }
 }

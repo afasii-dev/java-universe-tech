f1b6d5ae144283ab77f0d6fe75e8953bb956670c
Robiyakhon <robkuch123@gmail.com>
2022-03-08 13:37:19 +0300

    First commit

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Robbie";
+        int age = 18 ;
+        System.out.println(name+" "+"is "+age+" years old");
+    }
+
+}

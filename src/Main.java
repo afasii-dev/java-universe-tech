27a2f5835197dce9491b71280f5218e66e74daf2
Ibrohim9090 <abdullozodatjk@gmail.com>
2022-03-09 20:55:13 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello BBL!");
-        String name = "Tomiris";
+        String name = "Ibrohim";
         System.out.println(name);
     }
 }

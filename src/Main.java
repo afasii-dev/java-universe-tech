aa6f4ed3f1d623afd45764e32162ea03d83c064c
BBL09 <100301471+BBL09@users.noreply.github.com>
2022-03-09 17:30:37 +0300

    rebase master

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello BBL!");
-        String name = "Usmonjon";
+        String name = "BBL";
         System.out.println(name);
     }
 }

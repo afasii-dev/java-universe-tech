3b0e738ecc3f6cec3977794087a191e8790752e1
Rustam0330 <Rustam.Khodjaeff@gmail.com>
2022-03-08 22:58:19 -0500

    quick changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello BBL!");
         String name = "Rustam";
+        int age = 31;
         System.out.println(name);
     }
 }

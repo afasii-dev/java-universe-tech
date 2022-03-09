a2bf49ae75d3ce24b2473dae36b108cf8871a0ec
BBL09 <100301471+BBL09@users.noreply.github.com>
2022-03-09 15:05:04 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello BBL!");
-        String name = "Rustam";
-        System.out.println(name);
+        System.out.println("Hello BBL!-git");
     }
 }

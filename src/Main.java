479df62359260edd08f9f6995babe37a341840d6
Rustam0330 <Rustam.Khodjaeff@gmail.com>
2022-03-09 10:03:43 -0500

    Main.java edit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Zarif!");
-        String name = "Zarif";
+        System.out.println("Hello Rustam!");
+        String name = "Rustam";
         System.out.println(name);
     }
 }

8973dd3272384bf2d29609fe90c470f531554dc7
tomiris <tomirisismatzoda@gmail.com>
2022-03-09 14:08:20 +0500

    second change

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bye Bye!");
         int var1=8;
-        int var2=15
-        System.out.println(var1 + "человек" + var2 + "яблок");
+        int var2=15;
+        System.out.println(var1 + " человек " + var2 + " яблок ");
     }
 }

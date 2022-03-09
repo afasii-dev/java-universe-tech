8b87989d153a1b80d9cfe0a56a08533aac9d85cd
gulistone <zarif.rajabov@gmail.com>
2022-03-09 08:25:20 +0300

    added city name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello BBL!");
-        String name = "Rustam";
+        String name = "Zarif";
+        String city = "Dushanbe"
         System.out.println(name);
     }
 }

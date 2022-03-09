eabcbf370e05e82201f7b8478dda2805d9a84cea
Rustam0330 <Rustam.Khodjaeff@gmail.com>
2022-03-08 22:43:31 -0500

    variable info add

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello BBL!");
         String name = "Rustam";
-        System.out.println(name);
+        int age = 31;
+        System.out.println(name + age);
     }
 }

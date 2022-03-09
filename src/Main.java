3f4e65f6ad1429e86532668d1744d8b88ea468b4
Rustam0330 <Rustam.Khodjaeff@gmail.com>
2022-03-09 10:06:59 -0500

    fast changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello BBL!");
-        String name = "Rustam";
+        System.out.println("Hello Zarif!");
+        String name = "Zarif";
         System.out.println(name);
     }
 }

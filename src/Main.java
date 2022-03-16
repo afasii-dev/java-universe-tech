b93bd004995bb884986717389d14857fa3caa728
gulistone <zarif.rajabov@gmail.com>
2022-03-16 09:06:26 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello BBL!");
+        System.out.println("Hello World!");
         String name = "Rustam";
         System.out.println(name);
     }

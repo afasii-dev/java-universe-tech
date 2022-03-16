15095ad6104b6c280bdfd00a230fabc4e65c276e
gulistone <zarif.rajabov@gmail.com>
2022-03-16 09:15:13 +0300

    try to get conflict1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+<<<<<<< HEAD
+        System.out.println("Hello Earth!");
         String name = "Rustam";
         System.out.println(name);
+=======
+       String helloWorld = "Hello world!";
+>>>>>>> d12630b59508b449834a62513817c75454ef4d24
     }
 }

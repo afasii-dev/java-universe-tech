addf2b2a004fc3a401dd41dfd80e55d71cca8221
alexandr.fasii <afasii.dev@yahoo.com>
2022-03-16 22:13:06 +0200

    homework added

--- src/main/arrays/Arrays.java
+++ src/main/arrays/Arrays.java
@@ -1,27 +1,27 @@
-package main;
+package main.arrays;
 
 public class Arrays {
 
     public static void main(String[] args) {
-//        1) Массив - это переменная, в которую можно положить сразу несколько значений.
-//        2) Все элементы массива имеют одинаковый тип
-//        3) При создании пустого массива присваиваются значения в зависимости от типа данных
-//        4) Размер массива нельзя изменить после его создания
+//        1) Массив (Array) - это переменная, в которую можно положить сразу несколько значений.
+//        2) Все элементы массива имеют одинаковый тип.
+//        3) При создании пустого массива присваиваются значения в зависимости от типа данных.
+//        4) Размер массива нельзя изменить после его создания.
 
 
-        int[] intArray = {55, 14, 109, 453};
-//                       [0,  1,  2,   3  ];
+        int[] intArray = {55, 14, 109, 453}; // Значения
+//                       [0,  1,  2,   3  ];  индексы значений
 
         String[] stringArray = {"BBL", "Rustam", "SEGA", "Shahzod"};
 
 //      to get element at index 0:
-//        System.out.println(intArray[0]);
-//        System.out.println(stringArray[0]);
+        System.out.println("Первый элемент массива intArray : " + intArray[0]);
+        System.out.println("Первый элемент массива stringArray : " + stringArray[0]);
 
 
         boolean[] children = new boolean[2];
-//      boolean[] children = {false, false};
-//        System.out.println(children[0]);
+//      boolean[] children = {false, false}; элементы по умолчанию, когда создаем новый массив
+        System.out.println(children[0]);
 
 //        default values depending on variable type:
 //        int - 0
@@ -32,7 +32,7 @@ public class Arrays {
 
         int[] testArray = new int[3];
 //      int[] testArray = {0,0,0}
-//
+
 //        testArray[0] = 5;
 //        testArray[1] = 6;
 //        testArray[2] = 7;
@@ -54,7 +54,7 @@ public class Arrays {
             System.out.println(boolArray[i]);
         }
 
-//        boolArray[11] = true; - Index out of bounds
+//        boolArray[11] = true; - Index out of bounds exception
 
 
 //        -------------------------------
@@ -71,10 +71,12 @@ public class Arrays {
 //    equals();
 
 
-        String[] myArray = {"I", "Love", "Java"};
+        String[] myArray = {" I ", " Love ", " Java "};
         String string = "";
         for (int i = 0; i < myArray.length; i++) {
-            System.out.println(myArray[i].length());
+            System.out.println(myArray[i]);
+            myArray[i] = myArray[i].trim();
+            System.out.println(myArray[i]);
         }
     }
 }

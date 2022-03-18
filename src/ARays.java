434d4de575dbac0e95de3749505671d6f70895f4
Ibrohim9090 <abdullozodatjk@gmail.com>
2022-03-18 19:04:19 +0500

    homework

--- src/ARays.java
+++ src/ARays.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class ARays {
+public class ARays {
+    public static void main(String[] args) {
+        String[] myCountry = {"My", "country", "is", "Tajikistan"};
+        CountryLength(myCountry);
+    }
+
+    public static void CountryLength(String[] myCountry) {
+        System.out.println(myCountry.length);
+    }
+
 }

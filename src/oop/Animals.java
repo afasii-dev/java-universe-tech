8689fe7b9be5f4d1c798b12b8208ddf201f3442a
Ibrohim9090 <abdullozodatjk@gmail.com>
2022-04-01 14:12:50 +0500

    version 2

--- src/oop/Animals.java
+++ src/oop/Animals.java
@@ -1,11 +1,37 @@
 package oop;
 
 public abstract class Animals {
-    public String name;
-    public int age;
-    public int weight;
+    private String name;
+    private int age;
+    private int weight;
 
-    abstract  void voice();
+    abstract void voice();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String  newName) {
+        name = newName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int  newAge) {
+        age = newAge;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int  newWeight) {
+         weight = newWeight;
+    }
+
+
 
     public void go(){
         System.out.println("going");
--- src/oop/Test.java
+++ src/oop/Test.java
@@ -1,16 +1,16 @@
 package oop;
 
-public class test {
+public class Test {
     public static void main(String[] args){
         Cat cat = new Cat();
         Dog dog = new Dog();
         Bird bird = new Bird();
 
 
-        cat.name = ("pispis");
-        cat.age = 2;
-        cat.weight = 3;
-        System.out.println("My cat's name "+ cat.name + ". It is " + cat.age + " years old. "+ "weigth is " +cat.weight );
+        cat.setName("pispis");
+        cat.setAge(2);
+        cat.setWeight(3);
+        System.out.println("My cat's name "+ cat.getName() + ". It is " + cat.getAge() + " years old. "+ "weigth is " +cat.getWeight() );
         System.out.println("My cat can");
         cat.voice();
         cat.run();
@@ -19,10 +19,10 @@ public class test {
         System.out.println("--------------------------------");
 
 
-        dog.name = ("Layka");
-        dog.age = 2;
-        dog.weight = 13;
-        System.out.println("My dog's name "+ dog.name + ". It is " + dog.age + " years old. "+ "weigth is " +dog.weight );
+        dog.setName("Layka");
+        dog.setAge(2);
+        dog.setWeight(13);
+        System.out.println("My dog's name "+ dog.getName() + ". It is " + dog.getAge() + " years old. "+ "weigth is " +dog.getWeight() );
         System.out.println("My dog can");
         dog.voice();
         dog.run();
@@ -30,10 +30,10 @@ public class test {
         dog.stop();
         System.out.println("--------------------------------");
 
-        bird.name = ("Hirs");
-        bird.age = 8;
-        bird.weight = 150;
-        System.out.println("bird's name "+ bird.name + ". It is " + bird.age + " years old. "+ "weigth is " +bird.weight );
+        bird.setName("Hirs");
+        bird.setAge(8);
+        bird.setWeight(150);
+        System.out.println("bird's name "+ bird.getName() + ". It is " + bird.getAge() + " years old. "+ "weigth is " +bird.getWeight() );
         System.out.println("Bird can");
         bird.voice();
         bird.run();

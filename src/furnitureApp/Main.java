37efe57707a0751886092f67a711e19e3fe4c9df
alexandr.fasii <afasii.dev@yahoo.com>
2022-04-06 18:43:09 +0300

    refactor

--- src/furnitureApp/Main.java
+++ src/furnitureApp/Main.java
@@ -1,9 +1,19 @@
 package furnitureApp;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Chair chair = new Chair();
         chair.setIsCleaned(false);
         chair.clean();
+
+
+        ArrayList<Color> tableColors = new ArrayList<>();
+        tableColors.add(Color.RED);
+        tableColors.add(Color.YELLOW);
+        Table table = new Table();
+        table.setColors(tableColors);
+
     }
 }

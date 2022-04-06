7c1d7fab59d05c429473539f50059209ff75df00
gulistone <zarif.rajabov@gmail.com>
2022-04-06 17:17:58 +0300

    file task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-       String helloWorld = "Hello world!";
+
+    public static void main(String[] args) throws IOException {
+        String[] array = {"Beat", "Let", "Control", "The", "Body", "Your"};
+        FileWriter fileWriter = new FileWriter("songName.txt");
+        for (String string : array) {
+            fileWriter.write(string.concat("\n"));
+        }
+        fileWriter.close();
+
+        FileReader fileReader = new FileReader("songName.txt");
+        Scanner scanner = new Scanner(fileReader);
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
     }
+
 }

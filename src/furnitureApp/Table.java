497a828f3ec9fe66e169e2eb3efe163642812dce
sega-11 <sega111183@mail.ru>
2022-04-06 20:30:16 +0500

    ArrayListColor

--- src/furnitureApp/Table.java
+++ src/furnitureApp/Table.java
@@ -1,6 +1,18 @@
 package furnitureApp;
 
+import java.util.ArrayList;
+
 public class Table extends Furniture {
+
+    private ArrayList<Color> colors;
+
+    public ArrayList<Color> getColors() {
+        return colors;
+    }
+
+    public void setColors(ArrayList<Color> colors) {
+        this.colors = colors;
+    }
 
     @Override
     void clean() {
@@ -14,4 +26,5 @@ public class Table extends Furniture {
     boolean build() {
         return false;
     }
+
 }
